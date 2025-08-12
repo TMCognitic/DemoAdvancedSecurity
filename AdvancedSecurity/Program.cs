69c3a68435b8bf43bc44c3f799e5cfdb5b34b9f2
Thierry Morre <thierry.morre@cognitic.be>
2025-08-12 13:50:51 +0200

    Appel à la procédure via un utilisateur Sql Server

--- AdvancedSecurity/Program.cs
+++ AdvancedSecurity/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using BStorm.Tools.Database;
+using Microsoft.Data.SqlClient;
+
+const string CONNECTION_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdvancedSecurity;User Id=AppLogin; Password=Test1234=;Connect Timeout=60;Encrypt=True;Trust Server Certificate=True;";
+
+using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+{
+    connection.Open();
+
+    var result = connection.ExecuteReader("AppSchema.GetValues", dr => new { Id = (int)dr["Id"], Value = (string)dr["Value"] }, true);
+
+    foreach(var item in result)
+    {
+        Console.WriteLine(item);
+    }
+}
